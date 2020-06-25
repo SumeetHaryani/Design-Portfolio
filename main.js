0775fc339efb04458f3b240cba647728515c22cc
SumeetHaryani <sumeetharyani3@gmail.com>
2020-06-25 23:40:26 +0530

    add portfolio sections and media queries

---
+++ main.js
@@ -0,0 +1,7 @@
+const toggleButton = document.getElementsByClassName('nav__menu-toggle')[0];
+const navbarLinks = document.getElementsByClassName('nav__list')[0];
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active');
+});
+
